c5a1c6ebc3e4917b356654b616a20af8db9ef3b4
jay <jalenjarrett0406@gmail.com>
2025-10-22 11:14:02 -0400

    updated bubble assignment

--- main.c
+++ main.c
@@ -4,46 +4,93 @@
 
 int main() {
 
-    int size, arr[MAX_SIZE], arrCopy[MAX_SIZE];
+	int size, arr[MAX_SIZE], arrCopy[MAX_SIZE];
 
-    printf("How many numbers to sort?\n");
-    scanf("%d", &size);
+	printf("How many numbers to sort?\n");
+	scanf("%d", &size);
 
-    for (int i = 0; i < size; i++) {
-        printf("Enter number %d: ", i + 1);
-        scanf("%d", &arr[i]);
-        arrCopy[i] = arr[i];
-    }
+	for (int i = 0; i < size; i++) {
+		printf("Enter number %d: ", i + 1);
+		scanf("%d", &arr[i]);
+		arrCopy[i] = arr[i];
+	}
 
-    printf("\nArray before sorting:\n");
+	printf("\nArray before sorting:\n");
+	display_array(arr, size);
 
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+	printf("\n\n");
+	bubble_sort(arr, size);
 
     printf("\n\n");
+    selection_sort(arrCopy, size);
 
-    bubble_sort(arr, size);
-
-    return 0;
+	return 0;
 }
 
-int bubble_sort(int arr[], int size) {
+void copy_array(int destination[], int source[], int size) {
 
-    int bubble[size][size];
+}
 
-    printf("Using Bubble sort\n\n");
+void swap(int arr[], int index1, int index2) {
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j ++) {
-            bubble[i][j] = arr[i];
+    // printf("%d %d ", arr[index1], arr[index2]);
+
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+
+}
+
+void bubble_sort(int arr[], int size) {
+
+	int i;
+
+	printf("Using Bubble sort\n\n");
+
+    for (i = 0; i < size - 1; i++) {
+        printf("Iteration# %d\n", i + 1);
+
+        for (int j = 0; j < size - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                // printf("%d %d ", arr[j], arr[j + 1]);
+                display_array(arr, size);
+            }
+            
+            // printf("%d %d", arr[j], arr[j+1]);
+            printf("\n");
         }
+        
     }
+    printf("\nSorted array using Bubble Sort:\n");
+    display_array(arr, size);
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("%d ", bubble[i][j]);
+}
+
+void selection_sort(int arr[], int size) {
+
+    printf("Using Selection sort\n\n");
+
+    for (int i = 0; i < size - 1; i++) {
+        int min = arr[i];
+        printf("Iteration# %d\n", i + 1);
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < min)
+                // min = arr[j];
+        
+                swap(arr, min, arr[i + 1]);
         }
+        display_array(arr, size);
         printf("\n");
+        
     }
 
+}
+
+void display_array(int arr[], int size) {
+    
+    for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+    
 }
--- main.h
+++ main.h
@@ -1,1 +1,5 @@
-int bubble_sort(int arr[], int);
+void bubble_sort(int arr[], int);
+void swap(int arr[], int, int);
+void copy_array(int destination[], int source[], int);
+void selection_sort(int arr[], int);
+void display_array(int arr[], int);
